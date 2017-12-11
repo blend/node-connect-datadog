d12d45421d692af640b973d6ca8e6cf8368156d9
RandomSeeded <nate@blend.com>
2017-12-11 14:04:11 -0800

    more refactors and tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash');
 const expect = require('expect.js');
 
 const connectDatadog = require('../lib/index');
@@ -10,12 +11,12 @@ describe('connect datadog middleware', () => {
   let next;
   let nextCalled = false;
   beforeEach(() => {
-    req = {};
+    req = { route: { path: 'test' } };
     res = { end: () => { } };
     next = () => nextCalled = true;
   });
 
-  let histogramCalledWith;
+  let histogramCalledWith = { };
   const datadogMock = {
     increment: () => { },
     histogram: (stat, responseTime, sampleRate, tags) => {
@@ -25,15 +26,16 @@ describe('connect datadog middleware', () => {
 
   describe('middleware', () => {
     it('emits no events when there is no path set', () => {
+      const req = { };
       const middleware = connectDatadog.middleware({ dogstatsd: datadogMock });
       middleware(req, res, next);
       res.end();
       expect(nextCalled).to.be(true);
+      expect(histogramCalledWith).to.eql({});
     });
 
     it('attaches metadata', () => {
       const middleware = connectDatadog.middleware({ dogstatsd: datadogMock, path: true });
-      req.route = { path: 'test' };
       middleware(req, res, next);
       res.end();
       expect(histogramCalledWith.stat).to.be.ok();
@@ -44,9 +46,19 @@ describe('connect datadog middleware', () => {
   describe('generateStatTags', () => {
     const generateStatTags = connectDatadog.generateStatTags;
     it('attaches status:success on success', () => {
+      const statTags = generateStatTags(req, {
+        statusCode: 200,
+      }, { });
+      const statusTag = _.find(statTags, tag => /status/.test(tag));
+      expect(statusTag).to.be('status:success');
     });
 
     it('attaches status:error on error', () => {
+      const statTags = generateStatTags(req, {
+        statusCode: 404,
+      }, { });
+      const statusTag = _.find(statTags, tag => /status/.test(tag));
+      expect(statusTag).to.be('status:error');
     });
 
     it('attaches tags dynamically', () => {
